f1d205b4ef4d11a79164be1f7c39ac0b397e028d
truongnguyen1582000 <truongnguyen1582000>
2021-12-27 19:56:50 +0700

    change title browser

--- src/features/Event/pages/EventListPage.jsx
+++ src/features/Event/pages/EventListPage.jsx
@@ -2,10 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { getAllEvent } from '../../../api/eventApi';
 import EventList from '../components/EventList';
 import { CircularProgress } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function EventListPage(props) {
   const [eventList, setEventList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const user = useSelector((state) => state.user.current);
+  const isLoggedIn = !!user._id;
+  const navigate = useHistory();
 
   const refetchEvent = async () => {
     try {
@@ -19,6 +24,9 @@ function EventListPage(props) {
   };
 
   useEffect(() => {
+    if (!isLoggedIn) {
+      return navigate.push('/login');
+    }
     refetchEvent();
   }, []);
 
